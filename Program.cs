2a49f03494a4725e9ffde185cc1cafc0ce9849ef
noVentz <103551295+noVentz@users.noreply.github.com>
2022-04-13 18:41:22 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeFive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Vyachaslau";
+            string surName = "Matsialionak";
+            Console.WriteLine("Ваше фамилия и имя {0} {1}", name, surName);
+            string swap = name;
+            name = surName;
+            surName = swap;
+            Console.WriteLine("Ваше фамилия и имя {0} {1}", name, surName);
+        }
+    }
+}
